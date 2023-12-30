4ebafa525a6535da2dbf23a2fce27f78c8e8ba11
Michael McLean <g4m3rm1k3@hotmail.com>
2023-12-30 17:37:58 -0500

    send messages when events fired

--- src/main/java/org/michael/test/TestPlugin.java
+++ src/main/java/org/michael/test/TestPlugin.java
@@ -1,17 +1,31 @@
 package org.michael.test;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TestPlugin extends JavaPlugin {
+public final class TestPlugin extends JavaPlugin implements Listener
+{
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+
 
     }
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e){
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.RED + "Stop moving! You are frozen");
+    }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e){
+        e.getPlayer().sendMessage("You just threw an egg!");
     }
 }
